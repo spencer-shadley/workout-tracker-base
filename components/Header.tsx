39db80492cdb922464920d0215dd2530608543c6
Spencer Shadley <sshadley.dev@gmail.com>
2023-04-15 16:51:28 -0700

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <h1>Workout Tracker</h1>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/Header';
+
+export default function Home() {
+  const x = 1;
+  return (
+    <>
+      <p>{x}</p>
+      <Header />
+    </>
+  );
+}
