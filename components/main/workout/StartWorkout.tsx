bab0e0db8c2e7ecd7c6a14246a3fa4dd617eab17
Spencer Shadley <sshadley.dev@gmail.com>
2023-04-23 12:24:52 -0700

    added workout context

--- components/main/workout/StartWorkout.tsx
+++ components/main/workout/StartWorkout.tsx
@@ -3,21 +3,26 @@ import { useState } from 'react';
 import ExerciseColumn from './exercise/ExerciseColumn';
 import AutocompleteExercise from '@/components/shared/AutocompleteExercise';
 import { Button, Paper } from '@mui/material';
+import { createWorkoutContext } from './context/WorkoutContextProvider';
 
 export default function StartWorkout() {
   const [exercises] = useState<ExerciseInfo[]>([]);
 
+  const WorkoutContext = createWorkoutContext();
+
   return (
     <Paper sx={{ margin: 2, padding: 2 }} elevation={5}>
-      <AutocompleteExercise />
-      {exercises.length === 0 ? (
-        <>Add exercises to get started</>
-      ) : (
-        <>
-          <ExerciseColumn title={'Exercises'} />
-          <Button>Start workout</Button>
-        </>
-      )}
+      <WorkoutContext.Provider value={{ exercises, title: 'Workout' }}>
+        <AutocompleteExercise />
+        {exercises.length === 0 ? (
+          <>Add exercises to get started</>
+        ) : (
+          <>
+            <ExerciseColumn title={'Exercises'} />
+            <Button>Start workout</Button>
+          </>
+        )}
+      </WorkoutContext.Provider>
     </Paper>
   );
 }
