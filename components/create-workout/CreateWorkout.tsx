e1eb680742ee0d80890148dea5a71f600bf6bf4b
Spencer Shadley <sshadley.dev@gmail.com>
2023-04-29 15:11:00 -0700

    improved input field

--- components/create-workout/CreateWorkout.tsx
+++ components/create-workout/CreateWorkout.tsx
@@ -1,4 +1,10 @@
-import { Button, Paper, TextField, Typography } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { ChangeEvent, useEffect, useState } from 'react';
 import {
   CreateWorkoutProvider,
@@ -8,10 +14,36 @@ import {
 import useDebounce from '@/hooks/useDebounce';
 import searchExercises from '@/api/searchExercises';
 
+const hints = [
+  'Biceps',
+  'Bench press',
+  'Legs',
+  'Core',
+  'High intensity workout',
+  'Easy workout',
+  'Exercises that only require my body',
+  'Cardio workout',
+  'Exercises that only use dumbbells',
+];
+
+function getRandomHint() {
+  return `Try "${hints[Math.floor(Math.random() * hints.length)]}"`;
+}
+
 export default function CreateWorkout() {
   const [searchText, setSearchText] = useState<string>('');
   const [foundExercises, setFoundExercises] = useState<string[]>([]);
   const [isSearching, setIsSearching] = useState<boolean>(false);
+  const [currentHint, setCurrentHint] = useState<string>(getRandomHint());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentHint(getRandomHint());
+    }, 10000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const debouncedSearch = useDebounce<string>(searchText, 2000);
 
@@ -36,15 +68,26 @@ export default function CreateWorkout() {
   return (
     <CreateWorkoutProvider createWorkout={createWorkoutContext}>
       <Paper className="h-screen opacity-80 p-4">
-        {searchText === '' && <h1>Create Workout</h1>}
-        <TextField
-          className="w-full opacity-100"
-          value={searchText}
-          label="Search..."
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            setSearchText(event.target.value);
-          }}
-        />
+        {searchText === '' && <h1>Create an AI powered workout!</h1>}
+        <FormControl variant="outlined" fullWidth>
+          <TextField
+            maxRows={3}
+            multiline
+            id="outlined-adornment-search"
+            variant="outlined"
+            value={searchText}
+            label={currentHint}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              setSearchText(event.target.value);
+            }}
+            fullWidth
+            className="w-full opacity-100"
+            aria-describedby="create-workout-hint-text"
+            inputProps={{
+              'aria-label': 'hint',
+            }}
+          />
+        </FormControl>
         {searchText && <GenerateWithAiButton />}
         {isSearching ? <Typography>Searching...</Typography> : <Results />}
         {searchText && foundExercises.length === 0 && <NewExerciseButton />}
