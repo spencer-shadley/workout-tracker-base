772b7475dcdb5363968aa61686620a95fd99e1b3
Spencer Shadley <sshadley.dev@gmail.com>
2023-04-30 18:08:00 -0700

    added active workout link

--- components/create-workout/summary-dialog/SummaryDialogActions.tsx
+++ components/create-workout/summary-dialog/SummaryDialogActions.tsx
@@ -1,12 +1,15 @@
 import { Button, DialogActions } from '@mui/material';
 import { useSummaryDialogContext } from './context/SummaryDialogContextProvider';
+import Link from 'next/link';
 
 export function SummaryDialogActions() {
   const { close } = useSummaryDialogContext();
   return (
     <DialogActions>
       <Button onClick={close}>Close</Button>
-      <Button>Start Workout</Button>
+      <Link href="/ActiveWorkoutPage">
+        <Button>Start Workout</Button>
+      </Link>
     </DialogActions>
   );
 }
