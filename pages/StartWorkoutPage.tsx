8529b43587f7113c50d8d5a178947e7e60bc4852
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-01 10:37:41 -0700

    minor fix

--- pages/StartWorkoutPage.tsx
+++ pages/StartWorkoutPage.tsx
@@ -1,5 +1,4 @@
 import CreateWorkout from '@/components/create-workout/CreateWorkout';
-// import StartWorkoutWrapper from '@/components/main/create-workout/StartWorkoutWrapper';
 
 export default function StartWorkoutPage() {
   return <CreateWorkout />;
