7a5cadd546e8364ce4c107705d95484b8f97f2a5
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-01 13:49:25 -0700

    added everything back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,38 @@
 import '@/styles/globals.css';
-// import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from 'next-themes';
 
 import type { AppProps } from 'next/app';
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import tryInitFirebase from '@/utils/initFirebase';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import tryInitFirebase from '@/utils/initFirebase';
 
-// tryInitFirebase();
+tryInitFirebase();
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   console.log(Component, pageProps);
   return (
-    // <ThemeProvider>
-    //   <DndProvider backend={HTML5Backend}>
-    //     <QueryClientProvider client={queryClient}>
-    //       <div className="wave" />
-    //       <div className="wave" />
-    //       <div className="wave" />
-    //       <div className="wave" />
-    //       <div className="wave" />
-    <div className="wave" />
-    // {/* <Component {...pageProps} /> */}
-    // <ReactQueryDevtools initialIsOpen={false} />
-    //     </QueryClientProvider>
-    //   </DndProvider>
-    // </ThemeProvider>
+    <ThemeProvider>
+      <DndProvider backend={HTML5Backend}>
+        <QueryClientProvider client={queryClient}>
+          <div className="wave" />
+          <div className="wave" />
+          <div className="wave" />
+          <div className="wave" />
+          <div className="wave" />
+          <div className="wave" />
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </DndProvider>
+    </ThemeProvider>
   );
 }
