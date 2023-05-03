94aa0389ae4e0f3264012859211eb2b68c1621db
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-02 17:23:10 -0700

    reordered imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import BackgroundWaves from '@/components/shared/BackgroundWaves';
 
 const queryClient = new QueryClient();
