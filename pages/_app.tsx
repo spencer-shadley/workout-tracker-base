2292a6769684ff45dc4d71c5d9b864585f6ebdc4
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-04 22:01:12 -0700

    added dehydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import dayjs from 'dayjs';
@@ -17,10 +21,12 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <BackgroundWaves />
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Settings />
+      <Hydrate state={pageProps.dehydratedState}>
+        <BackgroundWaves />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Settings />
+      </Hydrate>
     </QueryClientProvider>
   );
 }
