74d468d623b7697671c48757a5a6251ca7cdd739
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-09 21:39:32 -0700

    added muscle group chips

--- components/main/workout/activity/card/ActivityCard.tsx
+++ components/main/workout/activity/card/ActivityCard.tsx
@@ -1,4 +1,11 @@
-import { Card, CardContent, LinearProgress, CardProps } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  LinearProgress,
+  CardProps,
+  Chip,
+  Skeleton,
+} from '@mui/material';
 import { useTimeContext } from '../../context/TimeContextProvider';
 import { useActivityCardContext } from '../../context/ActivityCardContextProvider';
 import { ActiveExercise } from './ActiveExercise';
@@ -7,6 +14,9 @@ import { ActivityCardCloseButton } from './ActivityCardCloseButton';
 
 import useActivityDurationInSeconds from '@/hooks/activity/useActivityDuration';
 import { VideoButtons } from './VideoButtons';
+import { useOpenAi } from '@/hooks/openai/useOpenAi';
+import { useEffect, useState } from 'react';
+import { random } from 'lodash';
 
 export function ActivityCard(cardProps: CardProps) {
   const { exerciseName, activityType } = useActivityCardContext();
@@ -35,6 +45,7 @@ export function ActivityCard(cardProps: CardProps) {
         <CardContent sx={{ flexGrow: 1 }}>
           <ExerciseTitle />
           <ActivityCardCloseButton />
+          <Chips />
         </CardContent>
         {isExerciseActive && <ActiveExercise />}
       </span>
@@ -44,4 +55,50 @@ export function ActivityCard(cardProps: CardProps) {
       )}
     </Card>
   );
+}
+
+function Chips() {
+  const { exerciseName, activityType } = useActivityCardContext();
+  const [muscles, setMuscles] = useState<string[]>([]);
+
+  const { data: muscleGroupsRaw, isFetching: isLoading } = useOpenAi({
+    prompt: `Provide a comma separated list of muscle groups that are relevant to the exercise ${exerciseName}. The return should be each muscle group followed by a comma, do not use "and" to separate muscle groups. Do not return more than the top 5 muscle groups. Return the muscle groups in order of relevance to the exercise. It is okay to return less than 5 muscle groups if there are fewer than 5 relevant muscle groups.`,
+  });
+
+  useEffect(() => {
+    if (muscleGroupsRaw) {
+      const muscleGroups = muscleGroupsRaw
+        .split(',')
+        .map((muscleGroup) => muscleGroup.trim());
+      setMuscles(muscleGroups);
+    }
+  }, [muscleGroupsRaw]);
+
+  if (activityType !== 'exercise') {
+    return null;
+  }
+
+  return (
+    <div className="flex gap-1">
+      {isLoading ? (
+        <ChipsSkeleton />
+      ) : (
+        muscles.map((muscle) => <Chip key={muscle} label={muscle} />)
+      )}
+    </div>
+  );
+}
+
+function ChipsSkeleton() {
+  const skeletons = [];
+  const randomNumberOfChips = random(1, 5);
+  for (let i = 0; i < randomNumberOfChips; i++) {
+    skeletons.push(
+      <Skeleton variant="rounded" sx={{ borderRadius: 100 }}>
+        <Chip label={'a'.repeat(random(3, 8))} />
+      </Skeleton>
+    );
+  }
+
+  return <div className="flex gap-1">{skeletons}</div>;
 }
