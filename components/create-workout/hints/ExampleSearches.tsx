dea25cdc0d1189e8995f841ece24fa0c3437a73b
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-10 15:19:00 -0700

    split search and generate buttons

--- components/create-workout/hints/ExampleSearches.tsx
+++ components/create-workout/hints/ExampleSearches.tsx
@@ -1,41 +1,49 @@
 import {
+  Button,
   Divider,
   List,
   ListItem,
-  ListItemButton,
   ListItemText,
   ListProps,
 } from '@mui/material';
 import { useCreateWorkoutContext } from '../context/CreateWorkoutContextProvider';
-import SwitchAccessShortcutIcon from '@mui/icons-material/SwitchAccessShortcut';
 import { randomHints } from './InitialSearchHint';
+import { AutoFixHigh, Search } from '@mui/icons-material';
+import { AiDialog } from '../summary-dialog/AiDialog';
+import { useState } from 'react';
 
 export function ExampleSearches(props: ListProps) {
   const { searchInput } = useCreateWorkoutContext();
   const { setSearchText } = searchInput;
+  const [showDialog, setShowDialog] = useState<boolean>(false);
 
   return (
     <List {...props}>
       {randomHints.map((hint, index) => (
         <div key={hint}>
-          <ListItem key={hint}>
-            <ListItemButton
+          <ListItem sx={{ display: 'flex' }} key={hint}>
+            <ListItemText sx={{ flexGrow: 1 }}>{hint}</ListItemText>
+            <Button
+              onClick={() => {
+                setShowDialog(true);
+              }}
+              endIcon={<AutoFixHigh />}
+            >
+              generate
+            </Button>
+            <Button
               onClick={() => {
                 setSearchText(hint);
               }}
+              endIcon={<Search />}
             >
-              <SwitchAccessShortcutIcon
-                sx={{
-                  marginRight: '10px',
-                  color: 'gray',
-                }}
-              />
-              <ListItemText>{hint}</ListItemText>
-            </ListItemButton>
+              Search
+            </Button>
           </ListItem>
           {index < randomHints.length - 1 && <Divider />}
         </div>
       ))}
+      <AiDialog showDialog={showDialog} setShowDialog={setShowDialog} />
     </List>
   );
 }
