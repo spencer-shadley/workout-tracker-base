df7afcc83eec7adcd005f5d76e14157c6d17ab9d
Spencer Shadley <sshadley.dev@gmail.com>
2023-05-28 22:35:16 -0700

    added tell me line

--- components/welcome/CustomizeToIndividual.tsx
+++ components/welcome/CustomizeToIndividual.tsx
@@ -1,10 +1,15 @@
-import { Button } from '@mui/material';
+import { Button, Card, Typography } from '@mui/material';
 
 import { useTutorialContext } from '../main/context/TutorialContext';
 
 export default function CustomizeToIndividual() {
   const { setStage } = useTutorialContext();
-  return <Button onClick={() => setStage(`complete`)}>
-    Continue
-  </Button>;
+  return <Card>
+    <Typography>
+      Tell me about yourself!
+    </Typography>
+    <Button onClick={() => setStage(`complete`)}>
+      Continue
+    </Button>
+  </Card>;
 }
