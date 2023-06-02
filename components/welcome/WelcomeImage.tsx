43fa9eb695ad094a94f1c0d048622b539cbc3aff
Spencer Shadley <sshadley.dev@gmail.com>
2023-06-02 11:37:41 -0700

    fixed indentation

--- components/welcome/WelcomeImage.tsx
+++ components/welcome/WelcomeImage.tsx
@@ -4,31 +4,31 @@ import { useOpenAi } from '@/api/hooks/openai/useOpenAi';
 import { Card, CircularProgress, Typography, useTheme } from '@mui/material';
 
 export function WelcomeImage() {
-    const theme = useTheme();
-    const prompt = `someone working out to improve their fitness in the style of impressionist art`;
-    const { data: imageUrl, isFetching } = useOpenAi({
-        prompt,
-        isPicture: true,
-        queryOptionOverrides: {
-            enabled: false
-        }
-    });
+  const theme = useTheme();
+  const prompt = `someone working out to improve their fitness in the style of impressionist art`;
+  const { data: imageUrl, isFetching } = useOpenAi({
+    prompt,
+    isPicture: true,
+    queryOptionOverrides: {
+      enabled: false
+    }
+  });
 
-    return <Card className='w-full flex justify-center' sx={{
-        backgroundColor: theme.palette.background.default,
-    }}>
-        {isFetching || !imageUrl ? <>
-            <CircularProgress />
-            <Typography>
-                Generating a unique custom image just for you
-            </Typography>
-        </> : <div className='w-1/2 h-1/2 pt-5 pb-0'>
-            <Image style={{ position: undefined }} src={`https://images.pexels.com/photos/2827392/pexels-photo-2827392.jpeg?cs=srgb&dl=pexels-zakaria-boumliha-2827392.jpg&fm=jpg`} fill alt="ai generated welcome picture" />
-            <Typography textAlign='center' variant='caption' color='secondary'>
-                AI Generated Image 🤖🖼️🔀
-                <br />
-                {`"${prompt}"`}
-            </Typography>
-        </div>}
-    </Card>;
+  return <Card className='w-full flex justify-center' sx={{
+    backgroundColor: theme.palette.background.default,
+  }}>
+    {isFetching || !imageUrl ? <>
+      <CircularProgress />
+      <Typography>
+        Generating a unique custom image just for you
+      </Typography>
+    </> : <div className='w-1/2 h-1/2 pt-5 pb-0'>
+      <Image style={{ position: undefined }} src={`https://images.pexels.com/photos/2827392/pexels-photo-2827392.jpeg?cs=srgb&dl=pexels-zakaria-boumliha-2827392.jpg&fm=jpg`} fill alt="ai generated welcome picture" />
+      <Typography textAlign='center' variant='caption' color='secondary'>
+        AI Generated Image 🤖🖼️🔀
+        <br />
+        {`"${prompt}"`}
+      </Typography>
+    </div>}
+  </Card>;
 }
